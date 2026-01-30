e24df8bd2e6d561a7baf10652eed9956c8b962fe
ucaeon <yoocw0426@gmail.com>
2026-01-31 00:59:08 +0900

    add: BOJ class2 files

---
+++ class2/1018.py
@@ -0,0 +1,22 @@
+h, w = map(int, input().split())
+data = []
+
+for i in range(h):
+    data.append(list(input()))
+
+# h-8, w-8까지 갯수 세어야함(이중포문)
+
+
+for i in range(h):
+    if data[0][0] == 'B':
+        for j in range(w):
+            if i % 0 == 2:
+                
+
+
+
+    else: 
+
+# for h, for w 이중 포문
+# if data[0][0] == b면
+    # 
