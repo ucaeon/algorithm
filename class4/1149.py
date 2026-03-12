fed0a317fc4797abe5bf7ae86bb776e8697e3d55
ucaeon <yoocw0426@gmail.com>
2026-03-12 20:33:40 +0900

    add: BOJ class4 files

---
+++ class4/1149.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
+
+dp = [[0] * 3 for _ in range(n)]
+
+dp[0][0] = data[0][0]
+dp[0][1] = data[0][1]
+dp[0][2] = data[0][2]
+
+for i in range(1, n):
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + data[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + data[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + data[i][2]
+    
+print(min(dp[n - 1]))
+
