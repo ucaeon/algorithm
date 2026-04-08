7940f8aad590ce0c64a7b385082f2035a4cbd897
ucaeon <yoocw0426@gmail.com>
2026-04-08 22:05:31 +0900

    update: BOJ class4 files

--- class4/1865.py
+++ class4/1865.py
@@ -1,3 +1,18 @@
+# 벨만포드
+def bellman_ford(n, data):
+    dist = [0] * (n + 1)
+
+    # 작으면 갱신(음수 사이클 찾기)
+    for i in range(n):
+        for a, b, c in data:
+            if dist[b] > dist[a] + c:
+                dist[b] = dist[a] + c
+                
+                # n반복이 되면? 음수 사이클 존재
+                if i == n - 1:
+                    return True
+    return False
+
 tc = int(input())
 
 result = []
@@ -6,26 +21,16 @@ for _ in range(tc):
     a, b, c = map(int, input().split())
     data = []
 
+    # 도로 저장(무방향으로)
     for i in range(b):
         k = list(map(int, input().split()))
         data.append((k[0], k[1], k[2]))
         data.append((k[1], k[0], k[2]))
 
+    # 웜홀 저장
     for i in range(c):
         k = list(map(int, input().split()))
         data.append((k[0], k[1], -k[2]))
-
-    def bellman_ford(n, data):
-        dist = [0] * (n + 1)
-
-        for i in range(n):
-            for a, b, c in data:
-                if dist[b] > dist[a] + c:
-                    dist[b] = dist[a] + c
-
-                    if i == n - 1:
-                        return True
-        return False
     
     if bellman_ford(a, data):
         result.append("YES")
